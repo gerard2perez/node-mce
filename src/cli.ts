ea5f1114da41e7d3b611d1478b5438461308b342
Gerardo Pérez <gerard2perez@outlook.com>
2018-06-14 15:08:29 -0500

    fix: self calling command

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,2 @@
 import { MCE } from "./index";
-// import { input, ask } from "./input";
-
-// (async ()=>{
-//     if (await ask('What to override the file?')) {
-//         console.log('Overriding');
-//     } else {
-//         console.log('no Overrid');
-//     }
-//     console.log(await input('What you really thing?'))
-// })();
-let command = MCE().subcommand(process.argv);
+MCE(__dirname).subcommand(process.argv);
