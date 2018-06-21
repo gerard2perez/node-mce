a9d0f1427e6c1ac38ca7589132caa645f1142d57
Gerardo Pérez <gerard2perez@outlook.com>
2018-06-20 19:18:47 -0500

    feat: add helper function to display created/updated files

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,23 @@
+import chalk from "chalk";
+import { ok } from "./console";
+import { targetPath } from "./paths";
+import { join } from "path";
+
 export function capitalize(text:string) {
     return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function fFile(...path:string[]) {
+	path.splice(0,0,targetPath());
+	let last:string = path.pop();
+	return (join(...path, chalk.green(last)).replace(/\\/mg, '/'));
+}
+export function created (text:string) {
+    let fpath = text.replace(targetPath(), '').replace(/\\/gm, '/').split('/');
+    ok(`\t${chalk.cyan('created')}: ${fFile(...fpath)}`);
+}
+
+export function updated (text:string) {
+    let fpath = text.replace(targetPath(), '').replace(/\\/gm, '/').split('/');
+    ok(`\t${chalk.cyan('updated')}: ${fFile(...fpath)}`);
 }
