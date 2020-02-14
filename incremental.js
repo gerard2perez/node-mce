55ffe36fbb85b8aec92a3962163db1f13bb5e7d8
Gerardo Pérez <gerard2perez@outlook.com>
2020-02-13 23:22:32 -0600

    fix: clean script will work correctly

--- incremental.js
+++ incremental.js
@@ -1,17 +1,19 @@
 const chokidar = require('chokidar');
 const glob = require('glob').sync;
+const chalk = require("chalk");
 const {watchFile,readFileSync, existsSync, unlinkSync} = require('fs');
 const { posix:{relative, join} } = require('path');
-chokidar.watch('./incremental.tsbuildinfo').on('change', (path, stats) => {
+function removeUnwanted(path, stats) {
     let {program:{fileInfos}} = JSON.parse(readFileSync('./incremental.tsbuildinfo'));
     let compiled = Object.keys(fileInfos).filter(f=>!f.includes('node_modules')).map(o=>relative('./src',o).replace('.ts', ''));
-    let sources = glob('lib/**/*.js').map(o=>relative('./lib', o.replace('.js', '')));
+    let sources = glob('lib/**/*.js', {ignore:['lib/node_modules/**/*', 'lib/templates/**/*'], dot:true}).map(o=>relative('./lib', o.replace('.js', '')));
     let fileToRemove = toLib(xor(sources, compiled));
     for(const file of fileToRemove) {
+        console.log(`[${chalk.gray(new Date().toLocaleTimeString())}] [${chalk.blue('INC.DEL')}]`, file);
         if(existsSync(file))unlinkSync(file);
     }
-
-  });
+  }
+chokidar.watch('./incremental.tsbuildinfo').on('change', removeUnwanted).on('add', removeUnwanted);
   function xor(arr1, arr2) {
     let arr3 = [...arr2, ...arr1].filter((v,i,a)=>(i===a.indexOf(v)));
     let result = [];
