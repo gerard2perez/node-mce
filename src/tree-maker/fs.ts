7d587bc4fbe3d442676155a277444bc4440eb469
gerard2perez <gerard2perez@outlook.com>
2020-11-02 22:02:36 -0600

    chore: function accepts TemplateStringsArray

--- src/tree-maker/fs.ts
+++ src/tree-maker/fs.ts
@@ -8,8 +8,23 @@ export interface fs_interface {
 	template:(...path:string[])=>string,
 	project:(...path:string[])=>string
 }
-export function highLightBasename(path:string, highlight:string) {
-	return chalk`${dirname(path)}/{${highlight} ${basename(path)}}`.replace(/\\/gm, posix.sep).replace(/^\.[\\\/]/, '');
+// export function highLightBasename2(text:TemplateStringsArray, ...values:any[]) {
+// 	console.log(text, values);
+// }
+export function highLightBasename(path:string, highlight:string):string;
+export function highLightBasename(text:TemplateStringsArray, ...values:any[]):string;
+export function highLightBasename(...data:any[]) {
+	let result = '';
+	if(data.length === 2 && typeof(data[0]) === 'string' && typeof(data[1]) === 'string' ) {
+		const [path, highlight] = data as [string, string];
+		result = chalk`${dirname(path)}/{${highlight} ${basename(path)}}`.replace(/\\/gm, posix.sep).replace(/^\.[\\\/]/, '');
+	} else {
+		const [stringarray, ...[directory]] = data as [TemplateStringsArray, ...string[]];
+		const dname = dirname(directory);
+		const bsname = basename(directory);
+		result = `${dname}/${chalk(stringarray, bsname)}`.replace(/\\/gm, posix.sep).replace(/^\.[\\\/]/, '');
+	}
+	return result;
 }
 function printHighLigthed(target:string) {
 	ok(highLightBasename(target, 'green.bold'));
