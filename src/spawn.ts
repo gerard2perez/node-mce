922daabc35c56a8080fb1213b0abac1ee5c10726
gerard2perez <gerard2perez@outlook.com>
2021-01-19 10:14:27 -0600

    feat: spawn a process that can capture TTY output

--- src/spawn.ts
+++ src/spawn.ts
@@ -1,6 +1,11 @@
 import cspawn from 'cross-spawn';
 import { SpawnOptions } from 'child_process';
 import { spin } from './spinner';
+import { WriteStream } from 'tty'
+/**
+ * 
+ * @deprecated use exec instead
+ */
 /* istanbul ignore next */
 export function spawn (cmd:string, options:any[], config:SpawnOptions, truefalse:boolean=true) {
     let buffer = '';
@@ -25,6 +30,10 @@ export function spawn (cmd:string, options:any[], config:SpawnOptions, truefalse
         child.stderr.on('data', store);
     });
 }
+/**
+ * 
+ * @deprecated use spin and exec instead
+ */
 // istanbul ignore next
 export function spinSpawn(message:string, cmd:string, options:any[], config:SpawnOptions={}) {
     return spin(message, async () =>{
@@ -32,3 +41,53 @@ export function spinSpawn(message:string, cmd:string, options:any[], config:Spaw
     });
 }
 export { cspawn as rawSpawn };
+export function exec(cmd: string, cmdOptions: string[], options: SpawnOptions = {}) {
+	return new LiveStream(cmd, cmdOptions, options)
+}
+
+type StreamEvent = (chunk: Buffer) => void
+class LiveStream {
+	#stdout = new WriteStream(1)
+	#stderr = new WriteStream(2)
+	#onData: StreamEvent = _ =>{}
+	#onError: StreamEvent= _ =>{}
+	#arguments: [string, string[], SpawnOptions]
+	output: Buffer = Buffer.allocUnsafe(0)
+	constructor(cmd: string, cmdOptions: string[], options: SpawnOptions = {}) {
+		
+		this.#arguments = [cmd, cmdOptions, {...{stdio: ['pipe', this.#stdout, this.#stderr]}, ...options}]
+	}
+	catchAndRelease(fn: StreamEvent, chunk: Buffer) {
+		this.output = Buffer.concat([this.output, chunk])
+		fn(chunk)
+	}
+	data(onData: StreamEvent) {
+		this.#onData = onData
+		return this
+	}
+	error(onError: StreamEvent = _ => {}) {
+		this.#onError = onError
+		return this
+	}
+	/**
+	 * Executes the program and captures the output using TTY if stdio is not set.
+	 * 
+	 * If you want to capture stderr you must call .error function first.
+	 */
+	async run(): Promise<Buffer> {
+		return new Promise((resolve, reject) => {
+			const child = cspawn(...this.#arguments)
+			if(this.#onError) this.#stdout.on('error', this.catchAndRelease.bind(this, this.#onError))
+			if(this.#onError) this.#stderr.on('error', this.catchAndRelease.bind(this, this.#onError))
+			this.#stdout.on('data', this.catchAndRelease.bind(this, this.#onData))
+			this.#stderr.on('data', this.catchAndRelease.bind(this, this.#onData))
+			child.once('close', (code, signal)=>{
+				if(code === 0) {
+					resolve(this.output)
+				} else {
+					reject(this.output)
+				}
+			})
+		})
+	}
+}
