21900f6dc9838c29193f3c6ef162780cf37d3eb6
gerard2perez <gerard2perez@outlook.com>
2021-01-24 19:56:58 -0600

    fix: prevents test to fail

--- src/system-streams.ts
+++ src/system-streams.ts
@@ -1,4 +1,4 @@
-import { Writable, Readable } from 'stream'
+import { Readable, Writable } from 'stream'
 import { MainSpinner } from './spinner'
 export const streams  = {
 	output: process.stderr,
@@ -7,5 +7,5 @@ export const streams  = {
 export function SetStreams(output: Writable, input: Readable) {
 	streams.output = output as any
 	streams.input = input as any
-	MainSpinner.stream = output
+	MainSpinner.stream = output as any
 }
