fb5fd1872fce4101bec404fc9d77956b3298ed80
gerard2perez <gerard2perez@outlook.com>
2021-01-27 04:10:12 -0600

    refact: migrates command to use new fs and tree-maker system and support dryrun

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1,20 +1,25 @@
-import { bool, enumeration, PackageJSON, Parsed, text } from '..'
+import { bool, enumeration, PackageJSON, Parsed, text, dry } from '..'
 import { error } from '../console'
 import { override } from '../input'
 import { information } from '../program'
-import { exec, LiveStream } from '../spawn'
+import { exec } from '../spawn'
 import { spin } from '../spinner'
 import { updateTextSpin } from '../spinner/console'
-import { c, d, match, PackageJSON2Chain, r, TreeMaker, w, z } from '../tree-maker'
-import { callerPath, cliPath } from '../tree-maker/fs'
-function thenOrCatch<T>(result: LiveStream, retVal: T) {
-	return result.data(chunck => {
-		updateTextSpin(chunck.toString())
-	})
-	.error()
-	.run()
-	.then(result => result.toString(), _ => retVal)
+import { cpy, dir, match, PackageJSON2Chain as pkg, wrt, cmp, root } from '../tree-maker'
+import { callerPath, cliPath, copy } from '../fs'
+import { SpawnOptions } from 'child_process'
+function dryExec<T>(cmd: string, cmdOptions: string[], options?: SpawnOptions) {
+	return function(retVal: T, wait_ms = 0, dryRunReturn = '') {
+		return exec(cmd, cmdOptions, options)
+			.data(chunck => updateTextSpin(chunck.toString()))
+			.error()
+			.dryRun(dryRunReturn, wait_ms)
+			.run()
+			.then(result => result.toString(), _ => retVal)
+	}
+	
 }
+
 enum Styles {
 	git = 'git',
 	single= 'single'
@@ -25,6 +30,7 @@ export const options = {
 	force: bool('-f', 'Overrides target directory'),
 	npm: bool('-n', 'Install npm dependencies'),
 	style: enumeration('-s <style>', 'Define the style of command you will use. If you need more than one command use git.', Styles, Styles.single),
+	dryRun: dry()
 }
 let nproy
 export const description = 'Creates a new MCE project.'
@@ -34,81 +40,75 @@ export  async function action(application: string, opt: Parsed<typeof options>) 
 	//istanbul ignore if
 	if(!await override('Directory already exist. Do you want to override it', nproy(), opt.force))
 		return
-	const tree = await createProjectExtructure(application, opt)
+	await createProjectExtructure(application, opt)
 	// istanbul ignore next
 	opt.npm && await spin('Initializing npm', async() => {
-		if ( await exec('npm', ['install', '-S'], {cwd: nproy()}).run().catch(() => false) === false ) {
+		if ( await dryExec('npm', ['install', '-S'], {cwd: nproy()})(false, 1000) === false ) {
 			error`npm installation failed`
 		}
 	})
 	await spin('Initializing git', async() => {
-		const initResult = await thenOrCatch(exec('git', ['init'], {cwd: nproy()}), false)
+		const initResult = await dryExec('git', ['init'], {cwd: nproy()})(false, 1000)
 		if ( initResult === false ) {
 			error`git init`
 		}
-		tree.w(c('gitignore', '.gitignore'))
+		copy('gitignore', `${application}/.gitignore`)
 	})
 }
 async function createProjectExtructure(application: string, opt: Parsed<typeof options>) {
-	let tree: TreeMaker
 	let {author} = opt
-	/* istanbul ignore else */
-	if(author === 'GIT_OR_NPM_USER') {
-		let git_user: any = await thenOrCatch(exec('git', ['config', 'user.name'], {}), '')
-		const git_email: any = await thenOrCatch(exec('git', ['config', 'user.email'], {}), '')
-		/* istanbul ignore else */
-		if(!git_user) git_user = await thenOrCatch(exec('npm', ['whoami'], {}), '')
-		author = `${git_user.trim()} <${git_email.trim()}>`
-	}
 	await spin('Creating Files', async () => {
+		/* istanbul ignore else */
+		if(author === 'GIT_OR_NPM_USER') {
+			let git_user: any = await dryExec('git', ['config', 'user.name'], {})('')
+			const git_email: any = await dryExec('git', ['config', 'user.email'], {})('')
+			/* istanbul ignore else */
+			if(!git_user) git_user = await dryExec('npm', ['whoami'], {})('')
+			author = `${git_user.trim()} <${git_email.trim()}>`
+		}
 		const cli = `import { MCE } from "@gerard2p/mce";\n\nMCE(__dirname).${opt.style === Styles.git ? 'subcommand':'command'}(process.argv);`
-		
 		const nPack = new PackageJSON(cliPath('templates', 'package.json'))
 			.withDefaults({
 				name: application,
 			})
 			.patchValues({
 				name: application,
-			description: '',
-			main: `./${application}`,
-			bin: {
-				[application]: `./${application}`,
-			},
-			keywords: [
-				application,
-				'cmd'
-			],
-			author,
-			license: 'MIT',
-			'scripts': {
-				'pkg-files': `node cpx "?(package.json|README.md|LICENSE|${application})"  lib`,
-			},
-			dependencies: {
-				'@gerard2p/mce': `^${information().version}`
-			}
-		})
-		tree = r(application)
-		tree.w(
-			c('app', application),
-			c('tsconfig.json'),
-			z('LICENSE', {application, year: new Date().getFullYear().toString(), author}),
-			z('README.md', {application})
-		)
-		.d('src',
-			w('cli.ts', cli),
-			match(opt.style === Styles.single, c('index.ts')),
-			match(opt.style === Styles.git, d('commands', c('../index.ts', 'removeme.ts'))),
-		)
-		.d('.vscode',
-			c('launch.json'),
-			c('settings.json'),
-		)
-		.w(	
-			PackageJSON2Chain(nPack),
-			c('incremental.js'),
-			c('cpx.js'),
-			c('jest.config.js')
+				description: '',
+				main: `./${application}`,
+				bin: {
+					[application]: `./${application}`,
+				},
+				keywords: [
+					application,
+					'cmd'
+				],
+				author,
+				license: 'MIT',
+				'scripts': {
+					'pkg-files': `node cpx "?(package.json|README.md|LICENSE|${application})"  lib`,
+				},
+				dependencies: {
+					'@gerard2p/mce': `^${information().version}`
+				}
+			})
+		root(application,
+			cpy('app', application),
+			cpy('tsconfig.json'),
+			cmp('LICENSE', {application, year: new Date().getFullYear().toString(), author}),
+			cmp('README.md', {application}),
+			dir('src',
+				wrt('cli.ts', cli),
+				match(opt.style === Styles.single, cpy('index.ts')),
+				match(opt.style === Styles.git, dir('commands', cpy('../index.ts', 'removeme.ts'))),
+			),
+			dir('.vscode',
+				cpy('launch.json'),
+				cpy('settings.json'),
+			),
+			pkg(nPack),
+			cpy('incremental.js'),
+			cpy('cpx.js'),
+			cpy('jest.config.js')
 		)
 	})
-	return tree
 }
