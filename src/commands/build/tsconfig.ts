83066d02d20b519a1d61d695c7e911a62659d7c6
gerard2perez <gerard2perez@outlook.com>
2021-02-16 21:18:13 -0600

    fix: tsconfig adds more type information and removes comments

--- src/commands/build/tsconfig.ts
+++ src/commands/build/tsconfig.ts
@@ -1,13 +1,23 @@
 import { readFileSync } from '../../mockable/fs'
 import { mergeDeep } from '../../@utils/merge-deep'
 import { callerPath } from '../../fs'
-
+export interface ITSConfig { 
+	compilerOptions: {
+		baseUrl?: string,
+		outDir?: string
+		paths?: {
+			[path: string]: string[]
+		}
+	}  
+}
 export function TSConfig(file: string) {
-	const tsConfig = JSON.parse(readFileSync(callerPath(file), 'utf-8'))
+	let raw = readFileSync(callerPath(file), 'utf-8')
+	raw = raw.replace(/(\/\*.*\*\/)/m, '')
+	const tsConfig = JSON.parse(raw)
 	let all = JSON.parse(JSON.stringify(tsConfig))
 	if(tsConfig.extends) {
 		const baseConfig = TSConfig(tsConfig.extends)
 		all = mergeDeep({}, tsConfig, baseConfig)
 	}
-	return all as { compilerOptions: {outDir: string }  }
+	return all as ITSConfig
 }
