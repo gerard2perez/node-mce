3c55afe12c6b5d170b434974551c5a8c64f8cdd1
gerard2perez <gerard2perez@outlook.com>
2021-02-17 11:56:12 -0600

    chore: migrates

--- src/commands/build/index.ts
+++ src/commands/build/index.ts
@@ -2,8 +2,7 @@ import { existsSync, unlinkSync } from '../../mockable/fs'
 import { bool, Parsed, text } from '../../core/options'
 import { exec } from '../../spawn'
 import { SyncFiles } from './sync'
-import { TSConfig } from './tsconfig'
-import { log } from '@gerard2p/mce/console'
+import { TSConfig } from '../../@utils/tsconfig'
 export const options = {
 	watch: bool('-w', 'watches for changes'),
 	tsc: text('-c', 'path to tsc', 'tsc'),
@@ -12,7 +11,7 @@ export const options = {
 export const args = '...[patterns]'
 export async function action(patterns: string[], opt: Parsed<typeof options>) {
 	const incrementalFile = './incremental.tsbuildinfo'
-	const TSCONFIG = TSConfig(opt.tsconfig)
+	const TSCONFIG = TSConfig(opt.tsconfig, true)
 	const builOptions = [opt.tsc, '--incremental', '--tsBuildInfoFile', incrementalFile, '-p', opt.tsconfig]
 	if(existsSync(incrementalFile)) unlinkSync(incrementalFile)
 	const { WatchIncremental } = await import('./incremental')
